37f9af4ae71c13e5228b575f34b83369905dbaa4
PawKanarek <paw.kanarek@gmail.com>
2023-12-04 12:49:34 +0000

    testing resource exhausted

--- examples/text_to_image/test_resource_exhausted.py
+++ examples/text_to_image/test_resource_exhausted.py
@@ -1,96 +1,46 @@
-import argparse
-import functools
-import logging
-import math
 import os
-import random
-from pathlib import Path
 
 import jax
+import jax.lib.xla_bridge
 import jax.numpy as jnp
+import jaxlib.xla_client
 import jaxlib.xla_extension
-import numpy as np
 import optax
-import torch
-import torch.utils.checkpoint
-import transformers
-from datasets import load_dataset
-from flax import jax_utils
+from flax import linen as nn
 from flax.training import train_state
-from flax.training.common_utils import shard
-from huggingface_hub import create_repo, upload_folder
-from torchvision import transforms
-from torchvision.transforms.functional import crop
-from tqdm.auto import tqdm
-from transformers import (
-    AutoTokenizer,
-    CLIPImageProcessor,
-    CLIPTokenizer,
-    FlaxCLIPTextModel,
-    FlaxCLIPTextModelWithProjection,
-    set_seed,
-)
-
-from diffusers import (
-    FlaxAutoencoderKL,
-    FlaxDDPMScheduler,
-    FlaxPNDMScheduler,
-    FlaxStableDiffusionXLPipeline,
-    FlaxUNet2DConditionModel,
-)
-from diffusers.utils import check_min_version
 
 
-def get_optimizer():
-    constant_scheduler = optax.constant_schedule(1e-5)
-    adamw = optax.adamw(learning_rate=constant_scheduler, b1=0.9, b2=0.999, eps=1e-08, weight_decay=1e-2)
-    optimizer = optax.chain(optax.clip_by_global_norm(1.0), adamw)
-    return optimizer
+class SingleLayerModel(nn.Module):
+    features: int
 
+    def setup(self):
+        self.dense = nn.Dense(self.features, kernel_init=nn.initializers.normal(), name="dense")
 
-def create_sd():
-    print("initialize sd goes smoothly")
-    unet, unet_params = FlaxUNet2DConditionModel.from_pretrained(
-        "duongna/stable-diffusion-v1-4-flax",
-        from_pt=False,
-        subfolder="unet",
-        dtype=jnp.bfloat16,
-    )
-
-    state = train_state.TrainState.create(apply_fn=unet.__call__, params=unet_params, tx=get_optimizer())
-    print("created state for sd2-1")
-    return state
-
-
-def create_sdxl():
-    print("initialize sdxl-base FlaxUNet2DConditionModel")
-    unet, unet_params = FlaxUNet2DConditionModel.from_pretrained(
-        "pcuenq/stable-diffusion-xl-base-1.0-flax",
-        from_pt=False,
-        subfolder="unet",
-        dtype=jnp.bfloat16,
-    )
-
-    print("******* TRYING TO CREATE TrainState WILL THROW RESOURCE_EXHAUSTED")
-    state = train_state.TrainState.create(apply_fn=unet.__call__, params=unet_params, tx=get_optimizer())
-    print("Never goes here :(")
-    return state
+    def __call__(self, x):
+        x = self.dense(x)
+        return x
 
 
 def main():
-    # I cannot create TrainState for SDXL on TPU v3-8 with 335 GB RAM
-    # create_sdxl will allocate about ~20GB of memory and then will throw error RESOURCE_EXHAUSTED
-    # It states that it have problem with allocating 50M of memory, despite fact that ~310GB is still free.
-    # try:
-    #     create_sdxl()
-    # except jaxlib.xla_extension.XlaRuntimeError as e:
-    #     # RESOURCE_EXHAUSTED: Error allocating device buffer: Attempting to allocate 50.00M. That was not possible. There are 27.86M free.; (0x0x0_HBM0)
-    #     print(e)
+    rng = jax.random.PRNGKey(0)
+    adamw = optax.adamw(learning_rate=optax.constant_schedule(1e-5))
 
-    # weird part: i can easly create 10 TrainStates for SD that will consume 200GB so i have free memory
-    sd_states = [create_sd() for _ in range(10)]
-    print(sd_states)
+    model = SingleLayerModel(features=128)
+    params = model.init(rng, jnp.ones((1, int(1e7))))
+
+    states = []
+    try:
+        while 1:
+            state = train_state.TrainState.create(apply_fn=model.apply, params=params, tx=adamw)
+            states.append(state)
+            jax.profiler.save_device_memory_profile(f"prof/memory_profile_{len(states)}.prof")
+            print(f"profiles created {len(states)}")
+    except jaxlib.xla_extension.XlaRuntimeError as e:
+        print(f"Memory exhausted. created {len(states)} states")
+        jax.profiler.save_device_memory_profile("prof/memory_profile_last.prof")
+        print(e)
 
 
 if __name__ == "__main__":
+    os.makedirs("prof", exist_ok=True)
     main()
--- examples/text_to_image/test_train_state_sdxl_flax.py
+++ examples/text_to_image/test_train_state_sdxl_flax.py
@@ -1,25 +1,29 @@
-import jax.numpy as jnp
+import os
+
+import jax.lib
+import jaxlib.xla_extension
 import optax
 from flax.training import train_state
 
 from diffusers import FlaxUNet2DConditionModel
 
 
-# I cannot create TrainState for SDXL on TPU v3-8 with 335 GB RAM
-# create_sdxl will allocate about ~20GB of memory and then will throw error RESOURCE_EXHAUSTED
-# It states that it have problem with allocating 50M of memory, despite fact that ~310GB is still free.
-print("initialize sdxl-base FlaxUNet2DConditionModel")
-unet, unet_params = FlaxUNet2DConditionModel.from_pretrained(
-    "pcuenq/stable-diffusion-xl-base-1.0-flax",
-    from_pt=False,
-    subfolder="unet",
-    dtype=jnp.bfloat16,
-)
-constant_scheduler = optax.constant_schedule(1e-5)
-adamw = optax.adamw(learning_rate=constant_scheduler, b1=0.9, b2=0.999, eps=1e-08, weight_decay=1e-2)
-optimizer = optax.chain(optax.clip_by_global_norm(1.0), adamw)
+def main():
+    jax.profiler.save_device_memory_profile(os.path.join("1.prof"))
+    unet, params = FlaxUNet2DConditionModel.from_pretrained(
+        "pcuenq/stable-diffusion-xl-base-1.0-flax",
+        subfolder="unet",
+    )
+    jax.profiler.save_device_memory_profile(os.path.join("2.prof"))
+    adamw = optax.adamw(learning_rate=optax.constant_schedule(1e-5))
+    try:
+        state = train_state.TrainState.create(apply_fn=unet.__call__, params=params, tx=adamw)
+        print(f"Never goes here :( {state=}")
+    except jaxlib.xla_extension.XlaRuntimeError as e:
+        # RESOURCE_EXHAUSTED: Error allocating device buffer: Attempting to allocate 50.00M. That was not possible. There are 27.86M free.; (0x0x0_HBM0)
+        print(e)
+    jax.profiler.save_device_memory_profile(os.path.join("3.prof"))
 
-print("******* TRYING TO CREATE TrainState AND THIS WILL THROW RESOURCE_EXHAUSTED")
-state = train_state.TrainState.create(apply_fn=unet.__call__, params=unet_params, tx=optimizer)
-# RESOURCE_EXHAUSTED: Error allocating device buffer: Attempting to allocate 50.00M. That was not possible. There are 27.86M free.; (0x0x0_HBM0)
-print(f"Never goes here :( {state=}")
+
+if __name__ == "__main__":
+    main()
