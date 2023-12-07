e2551090ace7718564de69076783438d92767429
PawKanarek <paw.kanarek@gmail.com>
2023-12-07 13:01:58 +0000

    tracing with tensorboard

--- examples/text_to_image/test_train_state_sdxl_flax.py
+++ examples/text_to_image/test_train_state_sdxl_flax.py
@@ -9,20 +9,22 @@ from diffusers import FlaxUNet2DConditionModel
 
 
 def main():
-    jax.profiler.save_device_memory_profile(os.path.join("1.prof"))
+    jax.profiler.start_trace("prf")
     unet, params = FlaxUNet2DConditionModel.from_pretrained(
         "pcuenq/stable-diffusion-xl-base-1.0-flax",
         subfolder="unet",
     )
-    jax.profiler.save_device_memory_profile(os.path.join("2.prof"))
     adamw = optax.adamw(learning_rate=optax.constant_schedule(1e-5))
     try:
-        state = train_state.TrainState.create(apply_fn=unet.__call__, params=params, tx=adamw)
-        print(f"Never goes here :( {state=}")
+        state = init_fn(unet, params, adamw)
     except jaxlib.xla_extension.XlaRuntimeError as e:
         # RESOURCE_EXHAUSTED: Error allocating device buffer: Attempting to allocate 50.00M. That was not possible. There are 27.86M free.; (0x0x0_HBM0)
         print(e)
-    jax.profiler.save_device_memory_profile(os.path.join("3.prof"))
+    jax.profiler.stop_trace()
+
+
+def init_fn(unet, params, adamw):
+    return train_state.TrainState.create(apply_fn=unet.__call__, params=params, tx=adamw)
 
 
 if __name__ == "__main__":
